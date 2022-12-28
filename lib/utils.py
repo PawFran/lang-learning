98aa83896186319ba4d023f6ecd41e241c2b8bf7
pawel franczak <pawel.franczak@comarch.com>
2022-12-28 18:13:19 +0100

    minor refactoring

--- lib/utils.py
+++ lib/utils.py
@@ -2,6 +2,7 @@ import re
 from numpy.random import default_rng
 
 
+# todo test it
 def extract_from_square_brackets(pattern, line):
     match = re.search(pattern, line)
     if match is not None:
@@ -11,6 +12,7 @@ def extract_from_square_brackets(pattern, line):
         return None
 
 
+# todo move it to dict class ?
 def random_dict_entry(dictionary, rng=default_rng()):
-    random_index = rng.integers(low=0, high=len(dictionary.entries))
+    random_index = rng.integers(low=0, high=dictionary.length())
     return dictionary.entries[random_index]
