b1ede7b48104b54a9573293a33b6419fbc8592f5
pawel franczak <pawel.franczak@comarch.com>
2022-12-28 17:33:54 +0100

    move parsing args to separate file

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,5 @@
 import re
 from numpy.random import default_rng
-from argparse import ArgumentParser
-import os
 
 
 def extract_from_square_brackets(pattern, line):
@@ -16,15 +14,3 @@ def extract_from_square_brackets(pattern, line):
 def random_dict_entry(dictionary, rng=default_rng()):
     random_index = rng.integers(low=0, high=len(dictionary.entries))
     return dictionary.entries[random_index]
-
-
-def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument('-l', '--language', action='store')
-    parser.add_argument('-r', '--remove', action='store_true')
-    return parser.parse_args()
-
-
-def parse_dict_path(language, dicts_folder):
-    dict_file_name = f'{language}.txt'
-    return os.path.join(dicts_folder, dict_file_name)
