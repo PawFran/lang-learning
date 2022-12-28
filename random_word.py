71afd295ee2e27001161de4a4f0fafd47b694476
pawel franczak <pawel.franczak@comarch.com>
2022-12-28 10:25:06 +0100

    possibility to choose a language via cli option

---
+++ random_word.py
@@ -0,0 +1,47 @@
+import os.path
+from argparse import ArgumentParser
+
+from lib.parsing_dict import *
+from lib.utils import *
+
+dicts_path = 'dicts'
+rng = default_rng()
+
+
+# todo option to remove words displayed already
+# todo or maybe option to decide after each word whether it should be removed or not
+# todo bug (add tests):
+# prop something up [phrasal
+# prop something up [phrasal verb]
+# They ran him up quick, and propped him out, over to leeward, and left him
+# todo start from certain index (and stop at certain)
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-l', '--language', action='store')
+    return parser.parse_args()
+
+
+def parse_dict_path(language):
+    dict_file_name = f'{language}.txt'
+    return os.path.join(dicts_path, dict_file_name)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    dict_path = parse_dict_path(args.language)
+
+    raw_lines = read_file_raw(dict_path)
+    dictionary = parse_english_dict(raw_lines) if args.language == 'english' else parse_latin_dict(raw_lines)
+
+    user_input = 'y'
+    while user_input.lower() != 'n':
+        random_entry = random_dict_entry(dictionary)
+        print(random_entry.head.base, end=' ')
+        input('')
+        print(random_entry.head.head_raw)
+        print(random_entry.example, end=' ')
+        input('')
+        for i in range(len(random_entry.translations)):
+            print(f'{i + 1}. {random_entry.translations[i]}')
+        user_input = input('\nProceed ? [y]/n\n')
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # random word from dictionary
 
 ## how to use it
-### python main.py
+### python random_word.py -l _language_ (english or latin - mandatory)
+### python random_word.py -h (for help)
 ### random words will appear on the console one by one until user will tell it to stop
