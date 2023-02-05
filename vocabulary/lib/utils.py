d0c94bcfc6e6b6eb15c54aed2b83432800953c54
pawel.franczak <pawel.fran@gmail.com>
2023-02-05 11:25:00 +0100

    fix weak_compare to handle uppercase

--- vocabulary/lib/utils.py
+++ vocabulary/lib/utils.py
@@ -23,4 +23,4 @@ def weak_compare(a, b):
     def replace_special(x):
         return x.replace('ā', 'a').replace('ē', 'e').replace('ī', 'i').replace('ō', 'o').replace('ū', 'u')
 
-    return replace_special(a).strip() == replace_special(b).strip()
+    return replace_special(a.lower()).strip() == replace_special(b.lower()).strip()
--- vocabulary/test/test_utils.py
+++ vocabulary/test/test_utils.py
@@ -9,3 +9,9 @@ def test_weak_compare():
     assert not weak_compare('castīgo', 'castīg')
     assert weak_compare('castāre', 'castare')
     assert weak_compare('valdē', 'valde')
+
+
+def test_wek_compare_uppercase():
+    assert weak_compare('Ī', 'ī')
+    assert weak_compare('ī', 'Ī')
+    assert weak_compare('Ī', 'I')
