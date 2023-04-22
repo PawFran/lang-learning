ed94655330cd4b15ac0f762c0bacbaf0f2b3292f
pawel.franczak <pawel.fran@gmail.com>
2023-04-22 11:37:44 +0200

    refactor

--- vocabulary/test/test_parsing_dict.py
+++ vocabulary/test/test_parsing_dict.py
@@ -1,12 +1,8 @@
-import os
-
 import pytest
 
-from vocabulary.lib.dict_classes import *
 from vocabulary.lib.parsing_dict import *
 
-test_files_dir = os.path.join('vocabulary/test/resources')
-
+test_files_dir = os.path.join('vocabulary', 'test', 'resources')
 
 test_files_name = 'latin_test.txt'
 dict_path = os.path.join(test_files_dir, test_files_name)
