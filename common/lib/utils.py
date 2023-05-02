e0187cfe43c214a9aef494a3446f34e7f3b36338
pawel.franczak <pawel.fran@gmail.com>
2023-05-02 17:32:26 +0200

    add short accents to replace_special

--- common/lib/utils.py
+++ common/lib/utils.py
@@ -1,6 +1,7 @@
 # treat letters with accents like the rest
 def weak_equals(a, b) -> bool:
     def replace_special(x):
-        return x.replace('ā', 'a').replace('ē', 'e').replace('ī', 'i').replace('ō', 'o').replace('ū', 'u')
+        return x.replace('ā', 'a').replace('ē', 'e').replace('ī', 'i').replace('ō', 'o').replace('ū', 'u')\
+                    .replace('ă', 'a').replace('ĕ', 'e').replace('ĭ', 'i').replace('ŏ', 'o').replace('ŭ', 'u')
 
     return replace_special(a.lower()).strip() == replace_special(b.lower()).strip()
