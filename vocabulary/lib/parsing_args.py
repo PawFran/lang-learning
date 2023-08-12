0d879a45733a77e95a21985b28465cb612a8421e
pawel.franczak <pawel.fran@gmail.com>
2023-08-12 22:19:12 +0200

    new worlds and ideas

--- vocabulary/lib/parsing_args.py
+++ vocabulary/lib/parsing_args.py
@@ -6,9 +6,10 @@ def parse_args():
     parser = ArgumentParser()
     parser.add_argument('-l', '--language', action='store')
     parser.add_argument('-r', '--remove', action='store_true')
-    parser.add_argument('-a', '--start_word', action='store')
-    parser.add_argument('-z', '--end_word', action='store')
+    parser.add_argument('-a', '--start_word', action='store')  # todo does it work with smart sampling ?
+    parser.add_argument('-z', '--end_word', action='store')  # todo does it work with smart sampling ?
     parser.add_argument('-u', '--user_name', action='store')
+    parser.add_argument('-t', '--top', action='store')
     return parser.parse_args()
 
 
