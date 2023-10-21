a2778e5abe1ff414d503cc2ac390286949da541f
pawfrk <pawel.fran@gmail.com>
2023-10-21 22:21:01 +0200

    add perf and supine exercise

---
+++ perf_and_supine_exercise.py
@@ -0,0 +1,46 @@
+from vocabulary.lib.parsing_dict import *
+from common.lib.utils import DEFAULT_USER_NAME
+
+if __name__ == '__main__':
+    rng = default_rng()
+
+    args = parse_args()
+
+    if args.language is None:
+        args.language = 'latin'
+        print(f'no language chosen. {args.language} will be used as default')
+
+    if args.user_name is None:
+        args.user_name = DEFAULT_USER_NAME
+
+    print(f'logged as {args.user_name}\n')
+
+    dictionary: Dictionary = parse_dictionary(args)\
+        .filter_by_simple_condition(['verb'])\
+        .filter_by_complex_condition('II|III|IV|V')
+
+    should_continue = True
+    while should_continue and dictionary is not None and dictionary.translations_nr() > 0:
+        verb: LatinVerb = dictionary.random_dict_entry(rng).head
+
+        word_original = verb.base
+        perfect = verb.perfect
+        supine = verb.supine
+
+        try:
+            perfect_answer = input(f'perf for {word_original}: ').strip()
+            perfect_is_correct = weak_equals(perfect_answer, perfect) if perfect_answer is not None else False
+
+            supine_answer = input(f'sup for {word_original}: ').strip()
+            supine_is_correct = weak_equals(supine_answer, supine) if supine_answer is not None else False
+
+            if perfect_is_correct and supine_is_correct:
+                print('both correct')
+            else:
+                print(f'at least one is wrong. correct answer is perf: {perfect} sup: {supine}')
+            print('')
+        except KeyboardInterrupt:
+            should_continue = False
+            print('\n')
+
+    print('terminating..')
