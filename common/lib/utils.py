1036e936e8286e4fef42b2fdee127174f9435099
pawfrk <pawel.fran@gmail.com>
2024-03-30 19:35:39 +0100

    refactor replace special

--- common/lib/utils.py
+++ common/lib/utils.py
@@ -1,13 +1,17 @@
 DEFAULT_USER_NAME = 'default_user'
 
 
-# accent agnostic
-def weak_equals(a, b, case_sensitive=False) -> bool:
-    def replace_special(x):
+def replace_special(x):
+    if x is not None:
         return x.replace('ā', 'a').replace('ē', 'e').replace('ī', 'i').replace('ō', 'o').replace('ū', 'u') \
             .replace('ă', 'a').replace('ĕ', 'e').replace('ĭ', 'i').replace('ŏ', 'o').replace('ŭ', 'u') \
             .replace('ë', 'e')
+    else:
+        return None
 
+
+# accent agnostic
+def weak_equals(a, b, case_sensitive=False) -> bool:
     if not case_sensitive:
         a = a.lower()
         b = b.lower()
