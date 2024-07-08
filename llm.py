c2f7782b7b6a203d20f4e1917c0371005cd6c371
pawfrk <pawel.fran@gmail.com>
2024-07-08 21:24:54 +0200

    chatgpt simple interface

---
+++ llm.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        raise Exception('must give at least one word')
+
+    llm = ChatOpenAI()
+
+    prompt = ' '.join(sys.argv[1:])
+
+    response = llm.invoke(prompt)
+
+    print(response.content)
