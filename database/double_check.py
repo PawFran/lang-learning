f422ba5abdb9e63dd8af4a37bf1dedb8742c504a
pawelfranczak <pawel.fran@gmail.com>
2024-11-30 11:00:37 +0100

    add note

--- database/double_check.py
+++ database/double_check.py
@@ -1,3 +1,4 @@
 # TODO number of nouns/verbs etc. from db vs file
 # TODO number of translations from db vs file
 # TODO number of words from db vs file
+# TODO odpytać widoki czy na pewno się stworzyły (nie leci bład jesli sie nie stworza)
