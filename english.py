d3b2c9438603af76c4a466acb900c50eeacc3a5b
pawelfranczak <pawel.fran@gmail.com>
2025-02-03 19:25:52 +0100

    use openai lib instead of langchain

--- english.py
+++ english.py
@@ -2,26 +2,38 @@
 
 import sys
 
-from langchain_openai import ChatOpenAI
+from openai import OpenAI
 
 output_temporary_file_name = 'scraping_out_tmp.txt'
 
 
 def llm_explain_word_dict_format(word, llm, f, sentence=None):
-    response = llm.invoke(
-        f'''explain word "{word}" using Cambridge Dictionary in the following format:
-                word [part of speech]
-                ()
-                1. explanation
-                ( if there is more than one explanation you can list them using subsequent numbers)
-    
-                I will give you some example for word \'battered\':
-                battered [adjective]
-                ()
-                1. hurt by being repeatedly hit
-                2. damaged, especially by being used a lot
-                '''
-    ).content + '\n'
+    prompt = f'''
+        explain word "{word}" using Cambridge Dictionary in the following format:
+        word [part of speech]
+        ()
+        1. explanation
+        ( if there is more than one explanation you can list them using subsequent numbers)
+
+        I will give you some example for word \'battered\':
+        battered [adjective]
+        ()
+        1. hurt by being repeatedly hit
+        2. damaged, especially by being used a lot
+        '''
+
+    completion = llm.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    response = completion.choices[0].message.content + '\n'
     if sentence is not None:
         response = fill_response_with_example(response, sentence)
     print(response)
@@ -41,7 +53,7 @@ if __name__ == '__main__':
 
     # TODO first try to find if the word is already present
 
-    llm = ChatOpenAI()
+    llm = OpenAI()
 
     # print('\n')
 
